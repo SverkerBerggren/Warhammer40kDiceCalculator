013b8f6273381f627bfe455ae541c04c591420d1
MrBoboGet <emanuelberggren01@gmail.com>
2022-08-14 01:17:22 +0200

    parser and download

--- app/src/main/java/com/example/warhammer40kdicecalculator/MainActivity.java
+++ app/src/main/java/com/example/warhammer40kdicecalculator/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.warhammer40kdicecalculator;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.function.Function;
+
+import com.example.warhammer40kdicecalculator.WahapediaUpdate;
+
+class Runnable_Function<FunctionArgument,FunctionReturnValue> implements Runnable
+{
+    Function<FunctionArgument,FunctionReturnValue> m_FunctionToRun = null;
+    FunctionArgument m_ArgumentToUse = null;
+    Runnable_Function(Function<FunctionArgument,FunctionReturnValue> FunctionToRun,FunctionArgument ArgumentToUse)
+    {
+        m_ArgumentToUse = ArgumentToUse;
+        m_FunctionToRun = FunctionToRun;
+    }
+    //@RequiresApi(api = Build.VERSION_CODES.N)
+    public void run()
+    {
+        m_FunctionToRun.apply(m_ArgumentToUse);
+    }
+}
+class Callback_Runner <RunArgumentType,RunResultType,CallbackResultType> implements Runnable
+{
+    Context m_AssociatedContext = null;
+    private Function<RunResultType,CallbackResultType> m_FunctionCallback = null;
+    private Function<RunArgumentType,RunResultType> m_CodeToRun = null;
+    private RunArgumentType m_RunArgument = null;
+    Callback_Runner(Context AssociatedContext, Function<RunResultType,CallbackResultType> Callback, Function<RunArgumentType,RunResultType> CodeToRun, RunArgumentType RunArgument)
+    {
+        m_AssociatedContext = AssociatedContext;
+        m_FunctionCallback = Callback;
+        m_CodeToRun = CodeToRun;
+        m_RunArgument = RunArgument;
+    }
+    public void run()
+    {
+        RunResultType RunReturnValue = m_CodeToRun.apply(m_RunArgument);
+        new Handler(m_AssociatedContext.getMainLooper()).post(new Runnable_Function<RunResultType,CallbackResultType>(m_FunctionCallback,RunReturnValue));
+    }
+}
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +62,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = getApplicationContext();
+        try {
+            InputStream Input = this.getAssets().open("TestParing.txt");
+            BattlescribeParser Parser = new BattlescribeParser();
+            ArrayList<BattlescribeUnit> Models = Parser.ParseUnits(Input);
 
+            int Hej2 = 0;
+        }
+        catch (Exception e)
+        {
+            String Error = e.getMessage();
+            int hej = 2;
+        }
+
+        EXAMPLEUPDATE();
     }
 
+    Integer p_UpdateCallback(String Result)
+    {
+        Toast.makeText(this,"Update result: "+Result,Toast.LENGTH_SHORT).show();
+        return(0);
+    }
+
+    void EXAMPLEUPDATE()
+    {
+        WahapediaUpdate Updater = new WahapediaUpdate();
+        UpdateArgumentStruct Arguments = new UpdateArgumentStruct();
+        Arguments.URLPrefix = "https://wahapedia.ru/wh40k9ed/factions/aeldari/";
+        Arguments.OutputPrefix = this.getDataDir().toString();
+        Arguments.FilesToDownload.add("Asurmen");
+        Arguments.FilesToDownload.add("Baharroth");
+
+        Thread UploadDataThread = new Thread(new Callback_Runner<UpdateArgumentStruct,String,Integer>(this,this::p_UpdateCallback,Updater::UpdateFiles,Arguments));
+        UploadDataThread.start();
+    }
 
     public void printTest(View v)
     {
