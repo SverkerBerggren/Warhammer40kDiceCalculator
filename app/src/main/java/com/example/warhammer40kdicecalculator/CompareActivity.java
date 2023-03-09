089be6b84f57175b03eca9f820da493bc3cd4b6d
SverkerBerggren <svartaspjutet@gmail.com>
2023-03-09 23:03:01 +0100

    Påbörjan med custom view

--- app/src/main/java/com/example/warhammer40kdicecalculator/CompareActivity.java
+++ app/src/main/java/com/example/warhammer40kdicecalculator/CompareActivity.java
@@ -1,8 +1,18 @@
 package com.example.warhammer40kdicecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.os.Bundle;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import java.util.HashMap;
 
 public class CompareActivity extends AppCompatActivity {
 
@@ -10,5 +20,57 @@ public class CompareActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compare);
+        LayoutInflater inflater =  getLayoutInflater();
+
+
+
+        View viewToModify = inflater.inflate(R.layout.unitviewprefab, findViewById(R.id.VerticalLayout));
+
+
+
+    }
+
+    private HashMap<String, DataSheet> datasheetMap = new HashMap<>();
+
+
+    private void createArmies()
+    {
+        
+    }
+
+
+
+    public void DropDownClick(View v)
+    {
+        Context context = v.getContext();
+
+
+        ConstraintLayout constraintLayout = (ConstraintLayout) v.getParent();
+
+        Log.d("Ui grejer", "Vad är höjden innan" +  constraintLayout.getMeasuredHeight());
+
+
+        ViewGroup viewGroup = (ViewGroup) constraintLayout;
+
+        for(int i = 0; i < viewGroup.getChildCount(); i++)
+        {
+            if(viewGroup.getChildAt(i) != v)
+            {
+                if(viewGroup.getChildAt(i).getVisibility() == View.VISIBLE)
+                {
+                    viewGroup.getChildAt(i).setVisibility(View.GONE);
+                }
+                else
+                {
+                    viewGroup.getChildAt(i).setVisibility(View.VISIBLE);
+                }
+            }
+        }
+
+        Log.d("Ui grejer", "Vad är höjden efter" +  constraintLayout.getMeasuredHeight());
+
+
+
+        Log.d("Ui grejer", "" +  v.getParent());
     }
 }
