67a12130fc17496dafe07e9f808eb694126e6e90
MarcusHedenberg <mackanhed@gmail.com>
2023-04-17 15:33:16 +0200

    Search

--- app/src/main/java/com/example/warhammer40kdicecalculator/Weapon_Popup.java
+++ app/src/main/java/com/example/warhammer40kdicecalculator/Weapon_Popup.java
@@ -4,20 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Weapon_Popup extends AppCompatActivity {
 
-
+    HashMap<String, Ability> map = new HashMap<String, Ability>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weapon_popup);
+
+
+    }
+
+    public void ShowPopup(View v)
+    {
+        View popup = findViewById(R.id.Popup);
+        popup.setVisibility(View.VISIBLE);
+    }
+
+    public void ClosePopup(View v)
+    {
+        View popup = findViewById(R.id.Popup);
+        popup.setVisibility(View.GONE);
+    }
+
+    public void ShowSearch(View v)
+    {
+
+        LayoutInflater inf = getLayoutInflater();
+        ViewGroup constraintLayout = findViewById(R.id.ConstraintLayout);
+        inf.inflate(R.layout.search_prefab, constraintLayout);
+
 
         View searchGroup = findViewById(R.id.SearchGroup);
         searchGroup.setVisibility(View.GONE);
@@ -26,13 +54,25 @@ public class Weapon_Popup extends AppCompatActivity {
         ListView listView = findViewById(R.id.listView);
 
         ArrayList<String> searchList = new ArrayList<String>(){{
-                add("Ability1");
-                add("Ability2");
-                add("Ability3");
+            add("Ability1");
+            add("Deal More Damage");
+            add("Take More Damage");
         }};
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,searchList);
+
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String item = (String)parent.getItemAtPosition(position);
+
+                Ability ability = map.get(item);
+
+            }
+        });
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -44,13 +84,12 @@ public class Weapon_Popup extends AppCompatActivity {
                 arrayAdapter.getFilter().filter(s);
                 return false;
             }
+
+
         });
 
-    }
 
-    public void ShowSearch(View v)
-    {
-        View searchGroup = findViewById(R.id.SearchGroup);
-        searchGroup.setVisibility(View.VISIBLE);
+        //View searchGroup = findViewById(R.id.SearchGroup);
+        //searchGroup.setVisibility(View.VISIBLE);
     }
 }
