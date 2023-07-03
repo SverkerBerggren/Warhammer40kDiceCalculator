db532ee0b7927a963de686616cd5f02fcf3791ea
SverkerBerggren <svartaspjutet@gmail.com>
2023-07-03 19:38:51 +0200

    Dialog prompt

--- app/src/main/java/com/example/warhammer40kdicecalculator/SavedMatchupsActivity.java
+++ app/src/main/java/com/example/warhammer40kdicecalculator/SavedMatchupsActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Dialog;
@@ -44,13 +45,17 @@ public class SavedMatchupsActivity extends AppCompatActivity {
     private Context context;
     private LinearLayout linearLayout;
 
+
+    private AlertDialogDeleteFragment alertFragment = null;
+
+    private FragmentManager fragmentManager = null;
     private int mStackLevel = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_matchups3);
 
-
+        fragmentManager = getSupportFragmentManager();
 
         context = getBaseContext();
 
@@ -135,39 +140,54 @@ public class SavedMatchupsActivity extends AppCompatActivity {
 
 
 
-        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(SavedMatchupsActivity.this);
-        alertBuilder.setMessage("Are you sure you want to delete this matchup?").setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+    //    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(SavedMatchupsActivity.this);
+    //    alertBuilder.setMessage("Are you sure you want to delete this matchup?").setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+    //        @Override
+    //        public void onClick(DialogInterface dialogInterface, int i) {
+//
+    //            ImageButton deleteButton = (ImageButton)linearLayout.findViewWithTag(v.getTag());
+    //            ConstraintLayout viewToRemove = (ConstraintLayout) deleteButton.getParent();
+//
+    //            linearLayout.removeView(viewToRemove);
+//
+    //            FileHandler fileHandler = new FileHandler(context);
+//
+    //            fileHandler.DeleteMatchup((String) v.getTag());
+//
+    //        }
+    //    }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+    //        @Override
+    //        public void onClick(DialogInterface dialogInterface, int i) {
+//
+    //        }
+    //    });
 
-                ImageButton deleteButton = (ImageButton)linearLayout.findViewWithTag(v.getTag());
-                ConstraintLayout viewToRemove = (ConstraintLayout) deleteButton.getParent();
+    //    AlertDialog dialog = alertBuilder.create();
+//
+    //    dialog.show();
 
-                linearLayout.removeView(viewToRemove);
+        Fragment prev = fragmentManager.findFragmentByTag("DeleteWindow");
+        if(prev == null)
+        {
+            AlertDialogDeleteFragment test = new AlertDialogDeleteFragment(this,(String) v.getTag());
 
-                FileHandler fileHandler = new FileHandler(context);
+            test.show(fragmentManager,"DeleteWindow");
+        }
+    //    prev = fragmentManager.findFragmentByTag((String) v.getTag());
+    //    prev = fragmentManager.findFragmentByTag((String) "DeleteWindow");
 
-                fileHandler.DeleteMatchup((String) v.getTag());
-
-            }
-        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-            }
-        });
-
-        AlertDialog dialog = alertBuilder.create();
-
-        dialog.show();
     }
 
-    private static class AlertDialogDeleteFragment extends DialogFragment
+    public static class AlertDialogDeleteFragment extends DialogFragment
     {
-
-        static AlertDialogDeleteFragment newInstance(int num) {
-            return new AlertDialogDeleteFragment();
+        private SavedMatchupsActivity savedMatchupsActivity = null;
+        private String tag = null;
+        public AlertDialogDeleteFragment(SavedMatchupsActivity savedMatchupsActivity, String tag)
+        {
+            this.savedMatchupsActivity = savedMatchupsActivity;
+            this.tag = tag;
         }
+
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getActivity());
@@ -176,7 +196,7 @@ public class SavedMatchupsActivity extends AppCompatActivity {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
 
-
+                    savedMatchupsActivity.DeleteMatchupCallback(tag);
 
                 }
             }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -185,9 +205,17 @@ public class SavedMatchupsActivity extends AppCompatActivity {
 
                 }
             });
-
             return alertBuilder.create();
         }
+    }
+
+    public void DeleteMatchupCallback(String tag)
+    {
+        ImageButton deleteButton = (ImageButton)linearLayout.findViewWithTag(tag);
+        ConstraintLayout viewToRemove = (ConstraintLayout) deleteButton.getParent();
+        linearLayout.removeView(viewToRemove);
+        FileHandler fileHandler = new FileHandler(context);
+        fileHandler.DeleteMatchup(tag);
     }
 
     public void OpenCompareActivity(View v)
